428dc5d8ac28766fb4de693d2d4f49880250b797
mikoker <mikolaj.kolata@gmail.com>
2025-07-10 14:47:36 +0200

    zizhan: Shim camera.xiaomi.so with libui-v34
    * Google changes for android 16 caused our camera hal to crash
    
    Change-Id: I604dc21808434a00c43b35a030e27f10836fb021

--- extract-files.py
+++ extract-files.py
@@ -32,8 +32,10 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/etc/camera/pureView_parameter.xml',): blob_fixup().regex_replace(
         r'=([0-9]+)>', r'="\1">'
     ),
+    'vendor/lib64/hw/camera.xiaomi.so': blob_fixup()
+    .add_needed('libprocessgroup_shim.so')
+    .replace_needed('libui.so', 'libui-v34.so'),
     (
-        'vendor/lib64/hw/camera.xiaomi.so',
         'vendor/lib64/hw/com.qti.chi.override.so',
         'vendor/lib64/libcamxcommonutils.so',
         'vendor/lib64/libmialgoengine.so',
