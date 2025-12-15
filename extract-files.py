fca3c207ab67b1a57bad96a0f3059ba31a94d1a3
Arian <arian.kulmer@web.de>
2025-12-15 10:36:44 +0100

    zizhan: Commonize GraphicBuffer blob fixup function
    Change-Id: If96a8ad9f76b87422be6ff9375d6f6fdf38c6491

--- extract-files.py
+++ extract-files.py
@@ -5,8 +5,6 @@
 #
 
 from extract_utils.fixups_blob import (
-    BlobFixupCtx,
-    File,
     blob_fixup,
     blob_fixups_user_type,
 )
@@ -17,12 +15,6 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
-from extract_utils.tools import (
-    llvm_objdump_path,
-)
-from extract_utils.utils import (
-    run_cmd,
-)
 
 namespace_imports = [
     'device/xiaomi/sm8450-common',
@@ -31,34 +23,6 @@ namespace_imports = [
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/xiaomi/sm8450-common',
 ]
-
-
-def blob_fixup_graphic_buffer_size(
-    ctx: BlobFixupCtx,
-    file: File,
-    file_path: str,
-    disassemble_symbols: [str],
-    *args,
-    **kwargs,
-):
-    for line in run_cmd(
-        [
-            llvm_objdump_path,
-            f'--disassemble-symbols={",".join(disassemble_symbols)}',
-            file_path,
-        ]
-    ).splitlines():
-        line = line.split(maxsplit=5)
-        if len(line) != 6:
-            continue
-
-        # The size of GraphicBuffer changed from 0x100 to 0xd30
-        offset, _, instruction, register, value, _ = line
-        if instruction == 'mov' and register[:-1] == 'w0' and value == '#0x100':
-            with open(file_path, 'rb+') as f:
-                f.seek(int(offset[:-1], 16))
-                f.write(b'\x00\xa6\x81\x52')  # AArch64 mov w0, #0xd30
-
 
 blob_fixups: blob_fixups_user_type = {
     (
@@ -70,8 +34,7 @@ blob_fixups: blob_fixups_user_type = {
     ),
     'vendor/lib64/hw/camera.xiaomi.so': blob_fixup()
     .add_needed('libprocessgroup_shim.so')
-    .call(
-        blob_fixup_graphic_buffer_size,
+    .update_graphic_buffer_size(
         [
             '_ZN5mihal9GraBufferC2EjjimNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE',
             '_ZN5mihal9GraBufferC2EPKNS_6StreamENSt3__112basic_stringIcNS4_11char_traitsIcEENS4_9allocatorIcEEEE',
